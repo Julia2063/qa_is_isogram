dc1a0a337e292aab530fe4099065f8c4564b7521
Julia2063 <sky206380@gmail.com.com>
2025-12-03 00:02:42 +0200

    Solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,34 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it('should returns true for a real isogram', () => {
+    const res = isIsogram('playgrounds');
+
+    expect(res).toBe(true);
+  });
+
+  it('should returns false for a non-isogram', () => {
+    const res = isIsogram('look');
+
+    expect(res).toBe(false);
+  });
+
+  it('should returns false when same letter appears in different cases', () => {
+    const res = isIsogram('Adam');
+
+    expect(res).toBe(false);
+  });
+
+  it('should returns true for empty string', () => {
+    const res = isIsogram('');
+
+    expect(res).toBe(true);
+  });
+
+  it('should returns false when same letter appears in different cases', () => {
+    const res = isIsogram('Oops');
+
+    expect(res).toBe(false);
+  });
 });
